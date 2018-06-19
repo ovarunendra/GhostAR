27de41a74199c30f5cce895e24f21e3153a3652a
ovarunendra <vpsingh016@gmail.com>
2018-06-19 14:00:55 +0530

    added react-native-router-flux

--- App.js
+++ App.js
@@ -1,10 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React from 'react';
 import {
   Platform,
   Dimensions,
@@ -13,67 +7,17 @@ import {
   TouchableOpacity,
   View
 } from 'react-native';
-import { RNCamera } from 'react-native-camera';
+import { Router, Stack, Scene } from 'react-native-router-flux';
+import LandingPage from './scenes/LandingPage';
+import CameraScene from './scenes/CameraScene';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const App = () => (
+  <Router>
+    <Stack key="root" hideNavBar >
+      <Scene key="landing" component={LandingPage} title="LandingPage" />
+      <Scene key="camera" component={CameraScene} title="CameraScene" />
+    </Stack>
+  </Router>
+);
 
-export default class App extends Component {
-  takePicture = async function() {
-    if (this.camera) {
-      const options = { quality: 0.5, base64: true };
-      const data = await this.camera.takePictureAsync(options)
-      console.log(data.uri);
-    }
-  };
-  render() {
-    return (
-      <View style={styles.container}>
-        <RNCamera
-            ref={ref => {
-              this.camera = ref;
-            }}
-            style = {styles.preview}
-            type={RNCamera.Constants.Type.back}
-            flashMode={RNCamera.Constants.FlashMode.on}
-            permissionDialogTitle={'Permission to use camera'}
-            permissionDialogMessage={'We need your permission to use your camera phone'}
-        />
-        <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center',}}>
-        <TouchableOpacity
-            onPress={this.takePicture.bind(this)}
-            style = {styles.capture}
-        >
-            <Text style={{fontSize: 14}}> SNAP </Text>
-        </TouchableOpacity>
-        </View>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: 'black'
-  },
-  preview: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center'
-  },
-  capture: {
-    flex: 0,
-    backgroundColor: '#fff',
-    borderRadius: 5,
-    padding: 15,
-    paddingHorizontal: 20,
-    alignSelf: 'center',
-    margin: 20
-  }
-});
+export default App;
